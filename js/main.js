da061c97636ea491aef736cc9cc3f407744f3f54
anatoliy-nik <dessid@yandex.ru>
2026-03-25 19:29:30 +0300

    nachal delat mobilu menu

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(function(){
         $('.burger-btn').toggleClass('burger-btn--active');
     });
 
+    /*---- слайдер брендов ----*/
     new Splide('#brands', {
         type : 'loop',
         // perPage: 3, /* Будет сжимать слайды, если сколько надо не влезут/ Не работает, если указан fixedWidth */
@@ -20,5 +21,21 @@ $(function(){
         pauseOnHover: true,
         pauseOnFocus: true
     }).mount();
+
+    /*---- 2-х уровневое меню (на мобиле) ----*/
+    $(document).on('click keydown', '.toggle-submenu', function(e) {
+        if (e.type === 'keydown' && e.which !== 13 && e.which !== 32) return;
+
+        e.preventDefault();
+        const $btn = $(this);
+        const $submenu = $btn.closest('.menu-list__item--submenu').find('.submenu-list');
+
+        // Закрываем другие подменю
+        $('.submenu-list.open').not($submenu).removeClass('open').siblings('div').find('.toggle-submenu').removeClass('open');
+
+        // Toggle текущего
+        $submenu.toggleClass('open');
+        $btn.toggleClass('open');
+    });
 
 });
